a57c6fd0a1f5025154122571b2cbe20e67fc6a1e
king1600 <3dsarcard@gmail.com>
2018-06-17 21:01:26 -0500

    io library + gitignore

---
+++ backend/src/io.c
@@ -0,0 +1,212 @@
+#include "io.h"
+
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+#define NUM_EPOLL_EVENTS 64
+#define READ_BUFFER_SIZE 4096
+
+static inline int change_event(io_loop_t* loop, int fd, int op, int events, void* data) {
+    // register an epoll event on the io loop's epoll handle
+    struct epoll_event event = { 0 };
+    event.data.fd = fd;
+    event.events = events;
+    event.data.ptr = data;
+    return epoll_ctl(loop->poll_fd, op, fd, &event);
+}
+
+static inline int create_server(int port) {
+    // create server socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+        return -1;
+
+    // create address to bind server to
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons((unsigned short) port);
+
+    // bind server address & prep for non blocking listening
+    int enable = 1;
+    fcntl(server_fd, F_SETFL, fcntl(server_fd, F_GETFL, 0) | O_NONBLOCK); // set non blocking
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)); // allow address reuse
+    if ((bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) || (listen(server_fd, 128) < 0)) {
+        close(server_fd);
+        return -1;
+    }
+
+    return server_fd;
+}
+
+//////////////////////////////////////////////////////
+
+bool io_client_write(io_client_t* client, const char* data, size_t size) {
+    ssize_t written = 0;
+    if (client->fd > 0) {
+
+        // make sure all bytes that can be written, are written
+        while (written > 0)
+            if ((written = write(client->fd, &data[written], size - written)) == -1)
+                break;
+
+        // close client if socket error
+        if (written == -1 && !(errno == EAGAIN || errno == EWOULDBLOCK)) {
+            io_client_close(client);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void io_client_close(io_client_t* client) {
+    // close socket and set client to closed
+    if (client->fd < 0)
+        return;
+    close(client->fd);
+    client->fd = -1;
+
+    // fix client list
+    io_client_list_t* client_list = &client->loop->client_list;
+    if (client == client_list->tail) {
+        client_list->tail = client->last;
+        client_list->tail->next = client->next;
+    }
+
+    // call callback & free client data
+    if (client->on_close)
+        client->on_close(client);
+    free(client);
+}
+
+bool io_loop_init(io_loop_t* loop, int port) {
+    // create epoll handle
+    if ((loop->poll_fd = epoll_create1(0)) < 0)
+        return false;
+
+    // create server socket
+    if ((loop->server_fd = create_server(port)) < 0) {
+        close(loop->poll_fd);
+        return false;
+    }
+
+    // prepare client list with values
+    io_client_t* head = &loop->client_list.head;
+    head->last = head->next = NULL;
+    loop->client_list.tail = head;
+
+    // register server socket to epoll handle
+    if (change_event(loop, loop->server_fd, EPOLL_CTL_ADD, EPOLLIN | EPOLLET, NULL) < 0) {
+        close(loop->server_fd);
+        close(loop->poll_fd);
+        return false;
+    }
+
+    return true;
+}
+
+void io_loop_iter_clients(io_loop_t* loop, io_client_iter_cb callback, void* state) {
+    // iterate over all active clients
+    for (io_client_t* client = loop->client_list.head.next; client != NULL; client = client->next)
+        callback(client, state);
+}
+
+int io_loop_run(io_loop_t* loop) {
+    io_client_t* client;
+    struct epoll_event event;
+    char read_buffer[READ_BUFFER_SIZE + 1];
+    struct epoll_event events[NUM_EPOLL_EVENTS];
+
+    // main event loop
+    while (true) {
+
+        // iterate through received io events
+        int num_events = epoll_wait(loop->poll_fd, events, NUM_EPOLL_EVENTS, -1);
+        while (num_events--) {
+            event = events[num_events];
+            client = (io_client_t*) event.data.ptr;
+
+            // close client (if any) on socket error
+            if ((event.events & (EPOLLERR | EPOLLHUP)) || !(event.events & EPOLLIN)) {
+                if (client != NULL)
+                    io_client_close(client);
+
+            // handle incoming clients on the server
+            } else if (event.data.fd == loop->server_fd) {
+                while (true) {
+                    
+                    // accept a client
+                    struct sockaddr addr;
+                    socklen_t addr_len = sizeof(addr);
+                    int client_fd = accept(loop->server_fd, &addr, &addr_len);
+                    if (client_fd == -1)
+                        break;
+
+                    // setup the client & create a client structure
+                    int enable = 1;
+                    fcntl(client_fd, F_SETFL, fcntl(client_fd, F_GETFL, 0) | O_NONBLOCK);
+                    setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &enable, sizeof(enable));
+                    io_client_t* client = malloc(sizeof(io_client_t));
+
+                    // bail and close all client data if not able to register to epoll handle
+                    if (change_event(loop, client_fd, EPOLL_CTL_ADD, EPOLLIN | EPOLLET, (void*) client) < 0) {
+                        free(client);
+                        close(client_fd);
+                        continue;
+                    }
+
+                    // load client values & add to servers client list
+                    client->loop = loop;
+                    client->next = NULL;
+                    client->fd = client_fd;
+                    client->on_read = NULL;
+                    client->on_close = NULL;
+                    client->last = loop->client_list.tail->last;
+                    loop->client_list.tail = loop->client_list.tail->next = client;
+
+                    // call connect callback on newly created client
+                    if (loop->on_connect)
+                        loop->on_connect(client);
+                }
+
+            // client has data to be read
+            } else if ((event.events & EPOLLIN) && client != NULL) {
+                ssize_t bytes = 1;
+                while (bytes > 0) {
+                    switch (bytes = read(client->fd, read_buffer, READ_BUFFER_SIZE)) {
+                    // client closed the connection
+                    case 0:
+                        bytes = 0;
+                        break;
+                    // no more data to read or error
+                    case -1:
+                        if (errno != EAGAIN)
+                            bytes = 0;
+                        break;
+                    // client read some data
+                    default:
+                        if (client->on_read) {
+                            read_buffer[bytes] = '\0';
+                            client->on_read(client, read_buffer, (size_t) bytes);
+                        }
+                    }
+                }
+
+                // check if connection was closed & close client
+                if (bytes == 0)
+                    io_client_close(client);
+            }
+        }
+    }
+
+    return 0;
+}
--- backend/src/main.c
+++ backend/src/main.c
@@ -1,6 +1,41 @@
+#include "io.h"
 #include <stdio.h>
+#include <string.h>
+
+static size_t clients_connected = 0;
+
+static void on_client_close(io_client_t* client) {
+    size_t client_id = (size_t) client->user_data;
+    printf("Client %lu disconnected\n", client_id);
+}
+
+static void on_client_read(io_client_t* client, const char* data, size_t len) {
+    size_t client_id = (size_t) client->user_data;
+    if (strstr(data, "\r\n\r\n") != NULL) {
+        printf("Client %lu received http packet\n", client_id);
+        const char* resp = "HTTP/1.1 200 Ok\r\nContent-Length: 11\r\nHello world\r\n\r\n";
+        io_client_write(client, resp, sizeof(resp) - 1);
+    }
+}
+
+static void on_client_connect(io_client_t* client) {
+    size_t client_id = clients_connected++;
+    client->user_data = (void*) client_id;
+    client->on_close = on_client_close;
+    client->on_read = on_client_read;
+    printf("Client %lu connected\n", client_id);
+}
 
 int main(int argc, char* argv[]) {
-    printf("Nothing to see here\n");
-    return 0;
+    io_loop_t loop;
+    int port = 12345;
+
+    if (!io_loop_init(&loop, port)) {
+        fprintf(stderr, "Failed to initialize io loop on port %d. Try a different port\n", port);
+        return -1;
+    }
+
+    printf("Server started on :%d\n", port);
+    loop.on_connect = on_client_connect;
+    return io_loop_run(&loop);
 }
